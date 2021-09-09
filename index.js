7e8bbd27111a8e483ce54a8d7aa26f1ad41b378b
openstudent <74672248+openstudent@users.noreply.github.com>
2021-09-10 00:13:34 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+http
+  .Server((req, res) => {
+
+    if (req.url === '/result4/') {
+      let CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+        'Access-Control-Allow-Headers':
+          'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
+      };
+
+      const result = {
+        message: 'gossoudarev',
+        'x-result': req.headers['x-test'],
+      };
+      let body = '';
+
+      req
+        .on('data', data => (body += data))
+        .on('end', () => {
+          result['x-body'] = body;
+          res.writeHead(200, {... CORS, 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(result));
+        });
+    }
+  })
+  .listen();
